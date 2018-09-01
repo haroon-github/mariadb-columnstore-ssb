314fc08864a20407f359e40fd6dff1ba4e3a8119
David Thompson <dthompson@mariadb.com>
2018-08-31 20:49:31 -0500

    Modify run-all-queries to clear cache and run 4 times Add sort helper script that will filter lineorder.tbl by year and create a new partially sorted (by year) file to test more real world scenario where dates tend to increase rather than be random as data generator produces

--- run-all-queries.sh
+++ run-all-queries.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 MCS_DIR=/usr/local/mariadb/columnstore
 for i in queries/q*.sql; do
-  $MCS_DIR/mysql/bin/mysql --defaults-extra-file=$MCS_DIR/mysql/my.cnf -u root -vvv ssb < $i
+  echo "==== $i.sql"
+  $MCS_DIR/mysql/bin/mysql --defaults-extra-file=$MCS_DIR/mysql/my.cnf -u root -e "select calFlushCache()"
+  for n in {1..4}; do
+    $MCS_DIR/mysql/bin/mysql --defaults-extra-file=$MCS_DIR/mysql/my.cnf -u root -vvv ssb < $i
+  done
 done
---
+++ sort_lineorder_by_date.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+read -d '' awkScript << 'EOF'
+{if($6>1998) {print > "lineorder_1998.tbl"}
+ else if ($6>1997) {print > "lineorder_1997.tbl"}
+ else if ($6>1996) {print > "lineorder_1996.tbl"}
+ else if ($6>1995) {print > "lineorder_1995.tbl"}
+ else if ($6>1994) {print > "lineorder_1994.tbl"}
+ else if ($6>1993) {print > "lineorder_1993.tbl"}
+ else if ($6>1992) {print > "lineorder_1992.tbl"}
+ else {print > "lineorder_other.tbl"}}
+EOF
+awk -F '|' "$awkScript" lineorder.tbl
+
+if [ -f "lineorder_other.tbl" ]; then
+  echo "ERROR: data generated to lineorder_other.tbl file which is not expected, please verify"
+  exit 1
+fi
+
+mv lineorder_1992.tbl lineorder_sorted.tbl
+for y in 1993 1994 1995 1996 1997 1998; do
+  cat lineorder_$y.tbl >> lineorder_sorted.tbl
+  rm -f lineorder_$y.tbl
+done
